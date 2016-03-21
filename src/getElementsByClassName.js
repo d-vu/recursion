2241ffb3aa333b93c2641e4d35270a05177e8af5
Daniel Vu <daniel.h.vu@gmail.com>
2016-03-21 15:27:00 -0500

    Created a helper recursive function within the function. I used element.children instead of element.childNodes to grab the children elements. getElementsByClassName complete.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,17 +2,25 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
 
-	//use following
-	//document.body, element.childNodes, element.classList
+  //create empty array
+  var elements = [];
+  //per testing spec get document.body element
+  var root = document.body;
+  //push document.body element into array
+  helper(root);
 
-	//create empty array
-	//per testing spec get document.body element
-	//push document.body element into array
-	//recursively push document.body child elements and grandchildren elements that contains 'className'
-
-	//return array
+  //recursively push child elements and subsequent grandchildren elements that contains 'className'
+  function helper(parent) {
+    if (parent.classList.contains(className)) {
+      elements.push(parent);
+    }
+    for (var i = 0; i < parent.children.length; i++) {
+      helper(parent.children[i]);
+    }
+  }
+  //return array
+  return elements;
 };
